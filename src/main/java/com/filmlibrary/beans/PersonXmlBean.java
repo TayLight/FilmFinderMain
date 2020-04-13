bfa2a49597a2e5ba67ce68c0f82d23926ef3597a
Denis <56925572+TayLight@users.noreply.github.com>
2020-04-13 19:34:28 +0400

    Создание xml

--- src/main/java/com/filmlibrary/beans/PersonXmlBean.java
+++ src/main/java/com/filmlibrary/beans/PersonXmlBean.java
@@ -1,9 +1,17 @@
 package com.filmlibrary.beans;
 
 import com.filmlibrary.Criterion;
+import com.filmlibrary.entities.EntityDB;
 import com.filmlibrary.entities.Person;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import javax.ejb.Stateless;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.time.LocalDate;
 import java.util.LinkedList;
@@ -12,10 +20,11 @@ import java.util.LinkedList;
 public class PersonXmlBean implements XmlBean {
 
     @Override
-    public void createXML(LinkedList<Criterion> criteria, LinkedList<Person> peoples) {
+    public void createXML(LinkedList<Criterion> criteria, LinkedList<EntityDB> peoples) throws ParserConfigurationException, TransformerException {
         LinkedList<Person> newPeople = new LinkedList<>();
-        for (Person people : peoples) {
+        for (EntityDB entity : peoples) {
             for (Criterion criterion : criteria) {
+                Person people = (Person) entity;
                 switch (criterion.getCriterion()) {
                     case ("id"):
                         if (people.getId() == Integer.parseInt(criterion.getValue()))
@@ -36,10 +45,33 @@ public class PersonXmlBean implements XmlBean {
             }
         }
         //TODO: тут преобразование списка в xml
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        Document doc = factory.newDocumentBuilder().newDocument();
+        Element root = doc.createElement("persons");
+       // root.setAttribute("type", "Person");
+        root.getAttributeNode("Person");
+        doc.appendChild(root);
+        File file = new File("src//main//java//com//filmlibrary//beans//xml//test.xml");
+
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.transform(new DOMSource(doc), new StreamResult(file));
     }
 
     @Override
     public void importXML(File xml) {
 
+    }
+
+    public static void main(String[] arg) throws TransformerException, ParserConfigurationException {
+        Criterion criterion1= new Criterion("id", "1");
+        Person person = new Person(1,"Стив","Куб", LocalDate.of(2002,8,12),"США");
+        LinkedList<Criterion> criteria = new LinkedList<>();
+        criteria.add(criterion1);
+        LinkedList<EntityDB> people = new LinkedList<>();
+        people.add(person);
+        PersonXmlBean personXmlBean = new PersonXmlBean();
+        personXmlBean.createXML(criteria,people);
     }
 }
--- src/main/java/com/filmlibrary/beans/XmlBean.java
+++ src/main/java/com/filmlibrary/beans/XmlBean.java
@@ -1,14 +1,18 @@
 package com.filmlibrary.beans;
 
 import com.filmlibrary.Criterion;
+import com.filmlibrary.entities.EntityDB;
 import com.filmlibrary.entities.Person;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.io.File;
 import java.util.LinkedList;
 
 public interface XmlBean {
 
-    void createXML(LinkedList<Criterion> criteria, LinkedList<Person> peoples);
+    void createXML(LinkedList<Criterion> criteria, LinkedList<EntityDB> entity) throws ParserConfigurationException, TransformerException;
 
     void importXML(File xml);
 }
